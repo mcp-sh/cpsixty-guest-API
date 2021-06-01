4b4becb7bd257951e800e3dd1c1f550fa4e29f18
Office - MCP <mcp@macapa.me>
2021-06-01 11:45:51 +0800

    Adding cookie to get Guest route

--- routes/api/guests.js
+++ routes/api/guests.js
@@ -10,6 +10,7 @@ router.get("/", async (req, res) => {
 router.get("/:guestID", async (req, res) => {
   const id = req.params.guestID;
   const guest = await Guest.findById(id).lean();
+  res.cookie("newUser", true);
   res.status(200).json(guest);
 });
 
